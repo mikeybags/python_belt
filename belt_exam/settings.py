5f865944306a46600c0f0aa488968385e46c653c
Michael Mitchell <mikeybags@gmail.com>
2017-01-27 22:33:38 -0600

    updated settings.py

--- belt_exam/settings.py
+++ belt_exam/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(j+q#1#)b2x13g3eqmwyxspu+qs^-f6v+&)*4f1oc(#27fe@s2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.201.68.63']
 
 
 # Application definition
